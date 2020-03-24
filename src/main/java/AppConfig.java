52e6698acdf109010ed5df4dd392cfa0b4d87f19
tommy.hamblin <tommy.hamblin@hotmail.co.uk>
2020-03-24 14:48:55 +0000

    Finishing touching for setting up the project properly
    * Includes making namespaces generic

--- src/main/java/AppConfig.java
+++ src/main/java/AppConfig.java
@@ -1,25 +1,53 @@
-import com.pluralsight.model.Speaker;
-import com.pluralsight.repository.HibernateSpeakerRepositoryImpl;
-import com.pluralsight.repository.ISpeakerRepository;
-import com.pluralsight.service.ISpeakerService;
-import com.pluralsight.service.SpeakerServiceImpl;
+/*
+ * Copyright 2020 Tommy Hamblin
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * you may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import com.spring.testbed.repository.Repository;
+import com.spring.testbed.repository.IRepository;
+import com.spring.testbed.service.IService;
+import com.spring.testbed.service.Service;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+/**
+ * The application configuration to wire up the Spring beans.
+ *
+ * @author tommy.hamblin.
+ */
 @Configuration
 public class AppConfig
 {
-    @Bean(name = "speakerService")
-    public ISpeakerService getSpeakerService()
+    /**
+     * The bean for the service layer.
+     *
+     * @return IService - instance of {@link IService}.
+     */
+    @Bean(name = "service")
+    public IService getService()
     {
-        SpeakerServiceImpl speakerService = new SpeakerServiceImpl();
-        speakerService.setRepository(this.getSpeakerRepository());
-        return speakerService;
+        return new Service(this.getRepository());
     }
 
-    @Bean(name = "speakerRepository")
-    public ISpeakerRepository getSpeakerRepository()
+    /**
+     * The bean for the repository layer.
+     *
+     * @return IRepository - instance of {@link IRepository}.
+     */
+    @Bean(name = "repository")
+    public IRepository getRepository()
     {
-        return new HibernateSpeakerRepositoryImpl();
+        return new Repository();
     }
 }
--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,16 +1,39 @@
-import com.pluralsight.service.ISpeakerService;
-import com.pluralsight.service.SpeakerServiceImpl;
+/*
+ * Copyright 2020 Tommy Hamblin
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * you may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import com.spring.testbed.service.IService;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+/**
+ * The runner class for the application.
+ */
 public class Application
 {
+    /**
+     * Main method to invoke the application.
+     *
+     * @param args - the runner arguments.
+     */
     public static void main(final String[] args)
     {
         ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
 
-        ISpeakerService service = applicationContext.getBean("speakerService", ISpeakerService.class);
+        IService service = applicationContext.getBean("service", IService.class);
 
-        System.out.println(service.findAll().get(0).getFirstName());
+        System.out.println(service.retrieveData().get(0).getFirstVariable());
     }
 }
